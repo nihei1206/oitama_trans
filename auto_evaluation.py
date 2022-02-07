f2321694bd9954ee75cc8eb19b0cee3f38083a0e
nihei1206 <nihei1206@gmail.com>
2022-02-07 20:05:10 +0900

    いらないimport消した

--- auto_evaluation.py
+++ auto_evaluation.py
@@ -1,25 +1,15 @@
-from enum import auto
-from turtle import update
 from sudachipy import tokenizer
 from sudachipy import dictionary
 from nltk.util import ngrams
 from nltk import bleu_score
-import collections
-import copy
-import math
-import sys
-import warnings
 from collections import Counter
 from fractions import Fraction
 import time
 import csv
-import pprint
 import datetime
 from tqdm import tqdm
 import numpy as np
-import numpy
 import random
-import statistics
 from statistics import mean
 import matplotlib.pyplot as plt
 
